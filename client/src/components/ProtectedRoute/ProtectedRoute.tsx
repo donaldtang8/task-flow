de6de2b6c4c89b047405fd462fe7ebcaa0a2d72d
donaldtang8 <donaldtang08@gmail.com>
2024-10-12 16:41:59 -0400

    Added protected route HOC

---
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import { useAuth } from "@/context/auth.context";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+
+const withProtectedRoute = (WrappedComponent: React.FC) => {
+    return (props: any) => {
+        const { isAuthenticated } = useAuth();
+        const router = useRouter();
+
+        useEffect(() => {
+            if (!isAuthenticated) {
+                router.push('/login');
+            }
+        }, [isAuthenticated, router]);
+
+        return isAuthenticated ? <WrappedComponent {...props} /> : null;
+    };
+};
+
+export default withProtectedRoute;
---
+++ client/src/pages/register/index.tsx
@@ -0,0 +1,9 @@
+import withProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
+
+const Register = () => {
+    return (
+        <div>Register</div>
+    )
+}
+
+export default withProtectedRoute(Register);
